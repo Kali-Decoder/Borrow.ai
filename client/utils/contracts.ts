69b8a75284f12418bc856dc5a8e80aaaf71b6820
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2024-07-24 17:09:15 +0530

    Change Types Settings

--- client/utils/contracts.ts
+++ client/utils/contracts.ts
@@ -2,7 +2,7 @@ import { client } from "@/app/client";
 import { getContract } from "thirdweb";
 import { sepolia, arbitrum, avalanche } from "thirdweb/chains";
 
-export const getContractInstance = async (address) => {
+export const getContractInstance = async (address:any) => {
   return getContract({
     client: client,
     chain: [sepolia, arbitrum, avalanche],
